4cb424c2eec939bf69e5e597a31729b0e4f58d9f
NoodleRobin <142407339+NoodleRobin@users.noreply.github.com>
2024-05-18 19:25:58 +0300

    Time: 37 ms (88.24%) | Memory: 17.9 MB (5.33%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,10 +1,20 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        answer = [str(i + 1) for i in range(n)]
-        for i in range(2, n, 3):
-            answer[i] = "Fizz"
-        for i in range(4, n, 5):
-            answer[i] = "Buzz"
-        for i in range(14, n, 15):
-            answer[i] = "FizzBuzz"
+        # answer = [str(i + 1) for i in range(n)]
+        # for i in range(2, n, 3):
+        #     answer[i] = "Fizz"
+        # for i in range(4, n, 5):
+        #     answer[i] = "Buzz"
+        # for i in range(14, n, 15):
+        #     answer[i] = "FizzBuzz"
+        answer = []
+        for i in range(1, n + 1):
+            currStr = ""
+            if i % 3 == 0:
+                currStr += "Fizz"
+            if i % 5 == 0:
+                currStr += "Buzz"
+            if not currStr:
+                currStr = str(i)
+            answer.append(currStr)
         return answer
