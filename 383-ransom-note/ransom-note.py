96f46e7e473749badd7befa5a43eba934bbcd006
Nadav Robinson <142407339+NoodleRobin@users.noreply.github.com>
2024-05-20 20:45:54 +0300

    Time: 66 ms (27.60%) | Memory: 16.6 MB (84.95%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -2,17 +2,14 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         my_dict = {}
         for char in magazine:
-            if char in my_dict:
-                my_dict[char] += 1
-            else:
+            if char not in my_dict:
                 my_dict[char] = 1
+            else:
+                my_dict[char] += 1 
         
         for char in ransomNote:
-            if char in my_dict:
-                if my_dict[char] == 1:
-                    del my_dict[char]
-                else:
-                    my_dict[char] -= 1
+            if char in my_dict and my_dict[char] > 0:
+                my_dict[char] -= 1
             else:
                 return False
         return True
