9334c154dfe88093b955b53b317ad42914a601d1
Nadav Robinson <142407339+NadavRobinson@users.noreply.github.com>
2025-01-23 11:02:50 +0200

    Time: 994 ms (58.17%) | Memory: 57.2 MB (98.69%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.py
+++ 1876-map-of-highest-peak/map-of-highest-peak.py
@@ -1,25 +1,32 @@
 class Solution:
-    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:    
-        m, n = len(isWater), len(isWater[0])
-        height = [[float('inf')] * n for _ in range(m)]
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        dx = [0, 0, 1, -1]
+        dy = [1, -1, 0, 0]
+        rows, cols = len(isWater), len(isWater[0])
+        height = [[-1] * cols for _ in range(rows)]
+        cellQueue = deque()
 
-        for i in range(m):
-            for j in range(n):
-                if isWater[i][j] == 1:
-                    height[i][j] = 0
-                else:
-                    if i > 0:
-                        height[i][j] = min(height[i][j], height[i - 1][j] + 1)
-                    if j > 0:
-                        height[i][j] = min(height[i][j], height[i][j - 1] + 1)
-
-        for i in range(m - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                if i < m - 1:
-                    height[i][j] = min(height[i][j], height[i + 1][j] + 1)
-                if j < n - 1:
-                    height[i][j] = min(height[i][j], height[i][j + 1] + 1)
-
+        for row in range(rows):
+            for col in range(cols):
+                if isWater[row][col] == 1:
+                    height[row][col] = 0
+                    cellQueue.append([row, col])
+        current_level = 1
+        while cellQueue:
+            queue_size = len(cellQueue)
+            for i in range(queue_size):
+                currentCell = cellQueue.popleft()
+                for i in range(4):
+                    neighborX = currentCell[0] + dx[i]
+                    neighborY = currentCell[1] + dy[i]
+                    if self.validCell(neighborX, neighborY, rows, cols) and height[neighborX][neighborY] == -1:
+                        height[neighborX][neighborY] = current_level
+                        cellQueue.append([neighborX, neighborY])
+            current_level += 1
+    
         return height
+    
+    def validCell(self, x, y, rows, cols):
+        return 0 <= x < rows and 0 <= y < cols
 
         
